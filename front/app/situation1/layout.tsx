5ddec74923061dbb3a8291e8e55d636f7e0b80cc
A-yum1 <ay7orange2ms@gmail.com>
2025-08-29 04:35:45 +0900

    送受信(仮)

--- front/app/situation1/layout.tsx
+++ front/app/situation1/layout.tsx
@@ -4,10 +4,8 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="ja">
       <body>
         {children}
       </body>
-    </html>
   );
 }
